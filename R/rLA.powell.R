3f7a38decc0167ab075be14d485a1c9c98023d5e
Jordan S Read <jread@usgs.gov>
2013-09-04 12:13:41 -0500

    added metrics, still constant wind

--- R/rLA.powell.R
+++ R/rLA.powell.R
@@ -1,16 +1,31 @@
 
+masl.offset	<-	1149
 root.dir	<-	'../data/'
 data	<-	read.table(paste(root.dir,'Powell.wtr.txt',sep=''),header=TRUE,sep='\t',
 	as.is=TRUE)
 require(rLakeAnalyzer)
 
-bathy.fPath	<-	paste(root.dir,'Wahweap_bathy.csv',sep='')
+bathy.fPath	<-	paste(root.dir,'Wahweap.bth.txt',sep='')
 Wahweap.bth	<-	load.bathy(bathy.fPath)
 
+names(Wahweap.bth)
 powell.lvl	<-	read.table(paste(root.dir,'Powell.lvl.txt',sep=''),header=TRUE,sep='\t')	
-
+names(powell.lvl)
+powell.lvl$Date	<-	as.Date(powell.lvl$Date,"%Y-%m-%d")
 un.dates	<-	sort(unique(data$Date))
-for (j in 1:10){#length(un.dates)){
+
+St	<-	vector(length=length(un.dates))
+time	<-	St
+uStar	<-	St
+LA	<-	St
+epi.temp	<-	St
+hypo.temp	<-	St
+srt.time	<-	St # sorter for time
+for (j in 1:length(un.dates)){
+	
+
+	pos.date	<-	as.Date(un.dates[j],"%m/%d/%Y %H:%M:%S")#"%Y-%m-%d %H:%M:%S"
+	
 	
 	# -- find dates matching single date
 	useI	<-	which(un.dates[j]==data$Date)
@@ -22,16 +37,30 @@ for (j in 1:10){#length(un.dates)){
 	meta.depths	<-	meta.depths(wtr[indx], depth[indx], slope=0.01, seasonal=TRUE)
 	
 	# -- interpolate temporally for surface height
-	
+	surf.masl	<-	approx(x=powell.lvl$Date,y=powell.lvl$masl,xout=pos.date,rule=2)$y
 		
-	# -- interpolate bth file for depths
-	# bthZ=0 is equivalent to 1149 masl
-	
-	
+	bthA	<-	Wahweap.bth$areas
 	# -- offset bathy for surface height
+	# (bthZ=0 is equivalent to 1149 masl)
+	bthZ	<-	Wahweap.bth$depths-(masl.offset-surf.masl)	# check this!!!!
 	
+	# -- interpolate bth file for depths
+	bthD	<-	c(0,bthZ[bthZ>0])
+	bthA	<-	approx(x=bthZ,y=bthA,xout=bthD)$y
+	epi.rho	<-	layer.density(0, meta.depths[1], wtr[indx], depth[indx], bthA, bthD)
+	epi.temp[j]	<-	layer.temperature(0, meta.depths[1], wtr[indx], depth[indx], bthA, bthD)
+	hypo.temp[j]<-	layer.temperature(meta.depths[2], max(bthD), wtr[indx], depth[indx], bthA, bthD)
+	hyp.rho	<-	layer.density(meta.depths[2], max(bthD), wtr[indx], depth[indx], bthA, bthD)
+	uStar[j]	<-	uStar(wndSpeed=2, wndHeight=10, epi.rho)
 	
-	schmidt.stability(wtr[indx], depth[indx], bthA, bthD)
-	print(meta.depths	)
-	#lake.number(bthA, bthD, uStar, St, meta.depths[1], meta.depths[2], averageHypoDense)
+	St[j]	<-	schmidt.stability(wtr[indx], depth[indx], bthA, bthD)
+	time[j]	<-	un.dates[j]
+	srt.time[j]	<-	pos.date
+	LA[j]	<-	lake.number(bthA, bthD, uStar[j], St[j], meta.depths[1], meta.depths[2], hyp.rho)
 }	
+
+indx	<-	order(srt.time)
+
+la.out	<-	data.frame(DateTime=time[indx],lake.number=LA[indx],Schmidt.Stability=St[indx],epi.temp=epi.temp[indx],hypo.temp=hypo.temp[indx])
+output = "../data/LakeAnalyzer_Powell.txt"
+write.table(la.out,file=output,col.names=TRUE, quote=FALSE, row.names=FALSE, sep="\t")
